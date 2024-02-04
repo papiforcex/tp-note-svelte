c7bf652391adbc6a3d9226431a14abea8eb4cc5a
Fayad <contact@papiforcex.com>
2024-02-04 18:47:09 +0100

    UPDATE - Début de la phase de conception de la page d'accueil

---
+++ app/src/lib/images/components/home/Play.svelte
@@ -0,0 +1,11 @@
+<!-- lib/components/home/Play.svelte -->
+<script>
+  // Export the 'Play' component if needed
+  export let Play;
+</script>
+
+<button>Heu</button>
+
+<style>
+  /* Component-specific styles here */
+</style>
